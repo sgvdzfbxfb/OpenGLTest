075998a75407a81f4e7caa2b53e37abab24c68a8
Charon <1692005543@qq.com>
2022-03-03 15:37:09 +0800

    简单光线追踪体验

---
+++ OpenGLTest/OpenGLTest/glError.hpp
@@ -0,0 +1,10 @@
+#ifndef OPENGL_CMAKE_SKELETON_GLERROR_HPP
+#define OPENGL_CMAKE_SKELETON_GLERROR_HPP
+
+// Ask Opengl for errors:
+// Result is printed on the standard output
+// usage :
+//      glCheckError(__FILE__,__LINE__);
+void glCheckError(const char* file, unsigned int line);
+
+#endif  // OPENGL_CMAKE_SKELETON_GLERROR_HPP
